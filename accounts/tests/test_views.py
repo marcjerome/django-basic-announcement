b994046fb5d06b0c6c6d89bc79c2e127a61a987a
marcjerome <marcjerometulali@gmail.com>
2020-07-24 22:51:44 +0800

    Testing login view and what comes next after it

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -4,9 +4,10 @@ class LoginPageTest(TestCase):
 
     def test_status_code_loginpage(self):
         response = self.client.get('/accounts/login')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 301)
 
     def test_loginpage_has_proper_template(self):
-        response = self.client.get('/accounts/login')
-        self.assertTemplateUsed(response, 'login.html')
-        
+        response = self.client.get('/accounts/login/')
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    
