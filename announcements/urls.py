c7279b8a150fbfe7745628ca29d955365d47709f
marcjerome <marcjerometulali@gmail.com>
2020-07-27 15:15:54 +0800

    Adding announcement feature success, Tests success for announcement adding

--- announcements/urls.py
+++ announcements/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, add_announcement
 
 urlpatterns = [
     path('', home, name='home'),
+    path('announcement/add/', add_announcement, name='add_announcement'),
 ]
--- announcements/views.py
+++ announcements/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import AnnouncementForm
 
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    form = AnnouncementForm()
+    return render(request, 'home.html', {'form': form})
+
+@login_required
+def add_announcement(request):
+    if request.method == 'POST':
+        form = AnnouncementForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return redirect('home')
