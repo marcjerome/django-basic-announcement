bce9b87ffabeeda670bfedba0923f8cf05e7a7b7
marcjerome <marcjerometulali@gmail.com>
2020-07-27 14:13:09 +0800

    Created models for announcements

--- announcements/models.py
+++ announcements/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Announcement(models.Model):
+    text = models.CharField(default='', max_length=500, help_text='What are you going to announce?')
+    date = models.DateField(auto_now=True, help_text='Date when announcement was created')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        unique_together = ['text', 'date']
