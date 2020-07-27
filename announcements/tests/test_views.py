c0011dc2e6f0e391eb570e49e94493ca8551f729
marcjerome <marcjerometulali@gmail.com>
2020-07-27 15:13:26 +0800

    Separated some tests in proper classes

--- announcements/tests/test_views.py
+++ announcements/tests/test_views.py
@@ -24,14 +24,21 @@ class HomeViewTest(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_can_save_post_request(self):
-        self.client.login(username='admin', password='pass@123')
-        response = self.client.post('announcement/add', data={'text': 'Website down'})
-        self.assertEqual(Announcement.objects.count(), 1)
-        new_announcement = Announcement.objects.first()
-        self.assertEqual(new_announcement.text, 'Website down')
-
     def test_announcement_form_in_view(self):
         self.client.login(username='admin', password='pass@123')
         response = self.client.get('/')
         self.assertIsInstance(response.context['form'], AnnouncementForm)
+
+class AnnouncementAddViewTest(TestCase):
+
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(username='admin', password='pass@123', email='admin@admin.com')
+        self.client = Client()
+
+    def test_can_save_post_request(self):
+        self.client.login(username='admin', password='pass@123')
+        self.client.post('/announcement/add/', data={'text': 'Website down'})
+        self.assertEqual(Announcement.objects.count(), 1)
+        new_announcement = Announcement.objects.first()
+        self.assertEqual(new_announcement.text, 'Website down')
